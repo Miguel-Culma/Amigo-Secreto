cd850958942b5cb0c1b88188a00c4054e23b25ec
Miguel Culma <miguel7angel1234@gmail.com>
2025-01-31 18:26:33 -0500

    Validar entrada y almacenar nombres

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresArray = [];
+
+function agregarAmigo(){
+    alert("Click al boton");
+    let nombreId = document.getElementById("amigo");
+    let nombre = nombreId.value;
+    nombre != "" ? nombresArray.push(nombre) :  alert("¡Añade el nombre de tu amigo!");
+    nombreId.value = "";
+}
+
